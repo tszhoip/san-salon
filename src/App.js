6fd349d7871077f6084b0c44b3c2a07d46ce3260
tsz ho ip <tszhoip@tszs-macbook.lan>
2021-06-17 18:45:23 -0700

    clean doc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        hi world
         </p>
         <a
           className="App-link"
@@ -15,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          YES
         </a>
       </header>
     </div>
