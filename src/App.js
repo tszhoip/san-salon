6d0e725e205a184f918546e3e7f682c1746e6e2b
tsz ho ip <tszhoip@tszs-macbook.lan>
2021-06-20 05:33:54 -0700

    css baseline

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg">
+      <h1>Experience Fulltime</h1>
+      <h2>Canoo, 18~present</h2>
+      <p>Currenly working at Canoo technology inc. as Design Lead in the Canoo Design Team, taking care of the overall website design & interaction. </p>
+      <h2>Noname.la, 15~present</h2>
+      <p>Currenly working at Canoo technology inc. as Design Lead in the Canoo Design Team, taking care of the overall website design & interaction. </p>
+      <p>Now this is where it gets ugly. In order to accurately align the base of all lines of text across all columns (which, of course, is one of the main points of having a baseline grid to start with), we have to offset the type manually. A simple way to do this is to add padding-top until the characters rest on the baseline and adjust the margin-bottom to reflect the offset.</p>
+      <p>Now this is where it gets ugly. In order to accurately align the base of all lines of text across all columns (which, of course, is one of the main points of having a baseline grid to start with), we have to offset the type manually. A simple way to do this is to add padding-top until the characters rest on the baseline and adjust the margin-bottom to reflect the offset.</p>
+      <p>Now this is where it gets ugly. In order to accurately align the base of all lines of text across all columns (which, of course, is one of the main points of having a baseline grid to start with), we have to offset the type manually. A simple way to do this is to add padding-top until the characters rest on the baseline and adjust the margin-bottom to reflect the offset.</p>
+ <ul>
+        <li>
+        Hello World
+        </li>
+        <li>
+        Hello World
+        </li>
+        <li>
+        Hello World
+        </li>
+      </ul>
+      
     </div>
   );
 }
