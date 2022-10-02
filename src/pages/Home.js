4bad823a4e5b378077e98e5fcd8adfc4219713b4
tszhoip <tszhoip@gmail.com>
2022-10-02 16:06:00 -0700

    clean up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Worklist = styled.div`
-
+const Banner = styled.div`
+width:100vw;
+height:80vh;
+min-height: 700px;
+background-image: url(https://sansalon.files.wordpress.com/2017/05/28e697b6e5b09ae5a686e5aeb9a41.jpg?w=1104);
+background-size: cover;
 `
 
 function Home() {
 
   return (
     <div>
+        <Banner/>
      <h1>sddddddddddd</h1>
      <h1>sdaffeffe</h1>
      <h1>sdaffeffe</h1>
